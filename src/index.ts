5ca579db47c4af4b06bec4ba44f4592a3ddc5e46
Chris Postma <cjrpostma@gmail.com>
2020-07-13 11:39:31 -0400

    Add bubble sort algorithm.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 class Sorter {
   constructor(public collection: number[]) {}
 
-  sort(): void {}
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const tempLeftSide = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = tempLeftSide;
+
+          // [this.collection[j], this.collection[j + 1]] = [
+          //   this.collection[j + 1],
+          //   this.collection[j],
+          // ];
+        }
+      }
+    }
+  }
 }
 
 const sorter = new Sorter([10, 3, -5, 0]);
