d957719d2901d6756f8de10c2bfe9fa9943b43db
Chris Postma <cjrpostma@gmail.com>
2020-07-13 14:31:07 -0400

    Remove Sortable interface from Sorter class.

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,8 +1,3 @@
-interface Sortable {
-  length: number;
-  compare(leftIndex: number, rightIndex: number): boolean;
-  swap(leftIndex: number, rightIndex: number): void;
-}
 export abstract class Sorter {
   abstract length: number;
   abstract compare(leftIndex: number, rightIndex: number): boolean;
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 import { LinkedList } from './LinkedListCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { NumbersCollection } from './NumbersCollection';
-import { Sorter } from './Sorter';
 
+// Linked List
 const linkedList = new LinkedList();
 linkedList.add(42);
 linkedList.add(-3);
 linkedList.add(7);
 linkedList.add(75);
 linkedList.add(0);
-
-const numberscollection = new NumbersCollection([-1, 6, 10, 3, -5, 0]);
-
-const charactersCollection = new CharactersCollection('faSdnvZXcVgeTYsdf');
-
-const sorter = new Sorter(linkedList);
-
-sorter.sort();
-
-// console.log(numberscollection.data);
-// console.log(charactersCollection.data);
+linkedList.sort();
 console.log(linkedList.print());
+
+// Number
+const numberscollection = new NumbersCollection([-1, 6, 10, 3, -5, 0]);
+numberscollection.sort();
+console.log(numberscollection.data);
+
+// String
+const charactersCollection = new CharactersCollection('faSdnvZXcVgeTYsdf');
+charactersCollection.sort();
+console.log(charactersCollection.data);
