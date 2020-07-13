10df2dee8298984274694857d09b71f41cbcc35f
Chris Postma <cjrpostma@gmail.com>
2020-07-13 13:46:53 -0400

    Add LinkedListCollection class.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
+import { CharactersCollection } from './CharactersCollection';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
 const numberscollection = new NumbersCollection([-1, 6, 10, 3, -5, 0]);
 
-const sorter = new Sorter(numberscollection);
+const charactersCollection = new CharactersCollection('faSdnvZXcVgeTYsdf');
+
+const sorter = new Sorter(charactersCollection);
 
 sorter.sort();
 
 console.log(numberscollection.data);
+console.log(charactersCollection.data);
