f84d708d1fa4f4c7a43f7ec7604ae580b8e93899
Chris Postma <cjrpostma@gmail.com>
2020-07-13 13:12:22 -0400

    Refactor Sorter class logic to be more geeric.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,10 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { NumbersCollection } from './NumbersCollection';
+import { Sorter } from './Sorter';
 
-  sort(): void {
-    const { length } = this.collection;
+const numberscollection = new NumbersCollection([-1, 6, 10, 3, -5, 0]);
 
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i; j++) {
-        // If collection is array of numbers
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            [this.collection[j], this.collection[j + 1]] = [
-              this.collection[j + 1],
-              this.collection[j],
-            ];
-          }
-        }
-        // If collection is an array of strings
-        if (typeof this.collection === 'string') {
-        }
-      }
-    }
-  }
-}
+const sorter = new Sorter(numberscollection);
 
-const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
-console.log(sorter.collection);
+
+console.log(numberscollection.data);
